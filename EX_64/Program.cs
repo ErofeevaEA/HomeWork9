a0a76ecab38736913e7c041c5a0ffda6236a812c
ErofeevaEvgeniya <golubenko.ewgenia@yandex.ru>
2022-09-08 22:15:12 +0300

    Exercise 64

---
+++ EX_64/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        Console.Write("Введи N: ");
+        int N = Convert.ToInt16(Console.ReadLine());
+        Console.Write("Введи M: ");
+        int M = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine($"N={N}, M={M}");
+        if (M > N)
+            for (int i = N; i <= M; i++)
+                Console.Write($" {i}");
+        else
+            for (int i = M; i <= N; i++)
+                Console.Write($" {i}");
+
+    }
+}
